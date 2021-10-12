29ae0a67fa2c8b393b52abfa5266cf18b904bdb4
Katie McConkey <2366393m@student.gla.ac.uk>
2021-10-12 14:45:16 +0100

    added map to home screen

--- mobile_game/lib/main.dart
+++ mobile_game/lib/main.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(55.875492878380975, -4.291251024554);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+ @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Mobile Game'),
+          title: const Text('Mobile App'),
+          backgroundColor: Colors.purple,
         ),
-       
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 15.0,
+          ),
+        ),
       ),
     );
   }
