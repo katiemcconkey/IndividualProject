f9c8ae184fcac3c91fefd43ea80191cd459c9ebf
Katie McConkey <2366393m@student.gla.ac.uk>
2021-10-12 16:55:09 +0100

    added pointer to current location usig coordinates

--- mobile_game/lib/main.dart
+++ mobile_game/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -10,14 +11,24 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final Set<Marker> _markers = {};
+
   late GoogleMapController mapController;
 
   final LatLng _center = const LatLng(55.875492878380975, -4.291251024554);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    setState(() {
+      _markers.add(Marker(
+        markerId: const MarkerId("1"),
+        position: _center,
+        infoWindow: const InfoWindow(title: "Your Location"),
+      ));
+    });
   }
- @override
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -31,6 +42,7 @@ class _MyAppState extends State<MyApp> {
             target: _center,
             zoom: 15.0,
           ),
+          markers: _markers,
         ),
       ),
     );
